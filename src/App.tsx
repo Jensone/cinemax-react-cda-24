834ae047b30da9f7fb3f873146277b631bf2cbb6
Jensone <hello@jensone.com>
2024-06-28 13:11:55 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+export default function App() {
+
+  const searchMovie = (e) => {
+    e.preventDefault()
+    console.log("La fonction searchMovie a été appelée")
+  }
+
+  return (
+      <main>
+        <h1>Cinemax</h1>
+        <h3>
+          Trouvez les infos de films et de séries en 2 clics !
+        </h3>
+        <form onSubmit={searchMovie}>
+          <input 
+          type="text"
+          className="border" />
+          <button
+          className="border"
+          >Rechercher</button>
+        </form>
+      </main>
+
+  )
+}
+
