2372066e4ba0a75576caf337dcb639a8cbc3535f
Jensone <hello@jensone.com>
2024-06-28 14:11:54 +0200

    ✨ App.tsx : Introduce animations using framer-motion library for smoother transitions 📝 index.css, output.css : Update background styling and button hover effects for better user experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 export default function App() {
 
@@ -9,18 +10,34 @@ export default function App() {
 
   return (
       <main>
-        <h1>Cinemax</h1>
-        <h3>
-          Trouvez les infos de films et de séries en 2 clics !
-        </h3>
-        <form onSubmit={searchMovie}>
-          <input 
-          type="text"
-          className="border" />
-          <button
-          className="border"
-          >Rechercher</button>
-        </form>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <h1>Cinemax</h1>
+        </motion.div>
+        <motion.div
+        
+        >
+        <h3>Trouvez les infos de films et de séries en 2 clics !</h3>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <form onSubmit={searchMovie}>
+            <input 
+            type="text"
+            />
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+              </svg>
+            </button>
+          </form>
+        </motion.div>
       </main>
 
   )
